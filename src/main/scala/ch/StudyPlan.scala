a849d571c9b79850ae079c8f0a94f17e52c287e3
David Kyrat <33601938+David-Kyrat@users.noreply.github.com>
2023-03-07 23:25:57 +0100

    added utility m,ethdos

--- src/main/scala/ch/StudyPlan.scala
+++ src/main/scala/ch/StudyPlan.scala
@@ -1,2 +1,8 @@
-final case class StudyPlan(val name: String, val year: Int, val faculty: String)
+package ch
+
+import ch.Req.studyPlanUrl
+
+final case class StudyPlan(val id: String, val name: String, val year: Int, val faculty: String) {
+  val request = f"$studyPlanUrl/$year-$id"
+}
 
