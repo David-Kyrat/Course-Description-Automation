e445c486bda66da2e2ed40d058d74596af9390dd
David Kyrat <33601938+David-Kyrat@users.noreply.github.com>
2023-03-09 09:58:29 +0100

    working on Course class factory method

--- src/main/scala/ch/Course.scala
+++ src/main/scala/ch/Course.scala
@@ -25,20 +25,32 @@ final case class Course(
   documentation: String,
   teachers: Vector[String],
   format: String,
-  studyPlan: Map[String, (Int, CourseType)],
+  studyPlan: Map[String, (Int, CourseType)]
 ) {
     val HoursequestHours = f"$courseUrl/$id-$year"
-    // TODO: implement retrieving fields from server response!
-    // NB: fields are mutable for now to simplify initialization, => This will be removed latter on.
 
+    /**
+     * We have to define an auxiliary constructor taking only 1 tuple
+     * argument to be able to do some json parsing before calling the constructor
+     * (in scala the first line of an auxiliary must always be a call to another constructor,)
+     *
+     * @param args
+     * @return
+     */
+    /* def this(args: (String, Int, String, Semester, String, String, String, String, String, String, CourseHours, String, Vector[String], String, Map[String, (Int, CourseType)])) = {
+
+    } */
+
+    // TODO implement retrieving fields from server response!
+    // NB fields are mutable for now to simplify initialization, => This will be removed latter on.
 
     // year in StudyPlan i.e 1->3 for Bachelor / 1->2 for Master ...
-    val spYear:Int =  id.head.toInt // first letter of course code, TODO: find smth that works also for master and phd
+    val spYear: Int = id.head.toInt // first letter of course code, TODO find smth that works also for master and phd
 
-    //var title = ???
-    //var hoursNb: CourseHours = ???
-    //var semester: Semester = ???
-    //val language: String = ???
+    // var title = ???
+    // var hoursNb: CourseHours = ???
+    // var semester: Semester = ???
+    // val language: String = ???
 
     // StudyPlan, Credits, IsMandatory Map (only string of studyplan otherwise we'll be stuck at construction since course Needs to have initialized Study plans and studyPlan needs initialized courses)
     // val StudyPlans: Map[String, Int, Boolean] = ???
@@ -57,6 +69,32 @@ final case class Course(
     /* var format: Option[_] = ???
     var preRequisites: Option[Vector[String]] = ???
     var usefulFor: Option[Vector[String]] = ??? */
+}
+
+object Course extends Function2[String, Int, Course] {
+    import com.google.gson.JsonObject
+
+    /**
+     * @param id String, i.e. course code, if `year` is not given => id must be the exact
+     * urlId (i.e. be of the form `year-code`, e.g. `2022-11X001`)
+     * @param year Int, year this course was given (optional)
+      * @return `JsonObject` that can be traversed like a Map with a `get()` method
+     */
+    def get(id: String, year: Int = 0): JsonObject = {
+        val reqUrl = if (year == 0) id else f"$year-$id"
+        val request: ReqHdl = ReqHdl.course(reqUrl)
+        request().jsonObj
+    }
+
+    private def factory(id: String, year: Int): Course = {
+        val jsonObj = get(id, year)
+
+        null
+    }
+
+    override def apply(id: String, year: Int): Course = factory(id, year)
+
+    println("Hello, world!")
 }
 
 sealed trait CourseType
--- src/main/scala/ch/StudyPlan.scala
+++ src/main/scala/ch/StudyPlan.scala
@@ -7,9 +7,9 @@ import ch.Utils.prettifyJson
  * Represents a Study Plan (i.e. Computer Science Bachelor)
  *
  * @param id String, immutable id of the studyPlan (i.e. the part without the year, that will never change, at least not supposed to)
- * @param year String, year the course in this studyplan are given
+ * @param year Int, year the course in this studyplan are given
  */
-final case class StudyPlan(val id: String, val year: String) {
+final case class StudyPlan(val id: String, val year: Int) {
     //val request = f"$studyPlanUrl/$year-$id"
     val urlId = f"$year-$id"
     // TODO: implement retrieving fields from server response
@@ -25,10 +25,10 @@ object StudyPlan {
     /**
      * @param id String, id of studyPlan, if `year` is not given => id must be the exact
      * url-id (i.e. be of the form `studyPlanYear-studyPlanUrlId`)
-     * @param year String year / version of this study plan (optional)
+     * @param year Int, year / version of this study plan (optional)
      * @return formatted Json response from server for details about given study plan
      */
-    def get(id: String, year: String = null) =
-        if (year == null) ReqHdl.studyPlan(id).get() else ReqHdl.studyPlan(f"$year-$id")
+    def get(id: String, year: Int = 0) =
+        if (year == 0) ReqHdl.studyPlan(id).get() else ReqHdl.studyPlan(f"$year-$id")
 
 }
