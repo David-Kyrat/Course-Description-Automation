20cbb61f85eedb0d19a4b6a5bb5e2c31792bf470
David Kyrat <33601938+David-Kyrat@users.noreply.github.com>
2023-03-16 11:18:21 +0100

    updated script

--- build.sbt
+++ build.sbt
@@ -16,6 +16,33 @@ scalaVersion := "2.13.10"
 name := "Course-Description-Automation"
 organization := "ch"
 
+// Packaging Config Infos
+enablePlugins(UniversalPlugin)
+enablePlugins(WindowsPlugin)
+
+maintainer := "Noah Munz <munz.no@gmail.com>"
+packageSummary := "test-windows"
+packageDescription := """Test Windows MSI."""
+
+// wix build information
+wixProductId := "ce07be71-510d-414a-92d4-dff47631848a"
+wixProductUpgradeId := "4552fb0e-e257-4dbd-9ecb-dba9dbacf424"
+
+// customize:
+
+/* mappings in Windows ++= (packageBin in Compile, sourceDirectory in Windows) map { (jar, dir) =>
+  Seq(jar -> "lib/cool.jar", (dir / "cool.bat") -> "bin/cool.bat")
+} */
+
+/* wixFeatures += WindowsFeature(
+    id="BinaryAndPath",
+    title="Project's Binaries and updated PATH settings",
+    desc="Update PATH environment variables (requires restart).",
+    components = Seq(
+      ComponentFile("bin/cool.bat"),
+      ComponentFile("lib/cool.jar"),
+      AddDirectoryToPath("bin"))
+) */
 
 // Want to use a published library in your project?
 // You can define other libraries as dependencies in your build like this:
@@ -23,6 +50,7 @@ organization := "ch"
 libraryDependencies += "org.scala-lang.modules" %% "scala-parser-combinators" % "2.1.1"
 libraryDependencies += "io.spray" %%  "spray-json" % "1.3.6"
 libraryDependencies += "com.google.code.gson" % "gson" % "2.10.1"
+
 // Here, `libraryDependencies` is a set of dependencies, and by using `+=`,
 // we're adding the scala-parser-combinators dependency to the set of dependencies
 
--- res/templates/fill-template_convert-pdf.ps1
+++ res/templates/fill-template_convert-pdf.ps1
@@ -1,24 +1,39 @@
 #TODO: SEE HOW TO RESOLVE PATH ONCE PACKAGED IN EXECUTABLE APP
 
-$projectHome = "~\DocumentsNb\BA4\Course-Description-Automation"
-$logFile = "$projectHome/res/log/pdf-convert.log"
-echo "" > $logFile # clean previous log messages or create the file
+$oldPath = $PWD
+$scriptPath = Split-Path $MyInvocation.MyCommand.Path -Parent
+# cd to where the script is so we actually know where the html template is relative to this script
+# the script must always be in a subfolder of the 'res' directory where the html template and 'log' directory is
 
+cd $scriptPath 
+$logFile = "../log/pdf-convert.log"
+echo "" > $logFile # clean previous log messages or create the file
 
 $template = "desc-template.html"  # html template to be filled each time
 
 $verbose = ($args -Contains "--verbose") 
 
-$in = $args[0] # Markdown file containing course data
-$in = (Resolve-Path $in -ErrorAction Continue).Path #2>> $logFile
-echo "in: $in"
+$in = "$oldPath\" + $args[0] # Markdown file containing course data
+$in_item = (Get-Item $in -ErrorAction Continue <#2>> $logFile#>) 
+$in = $in_item.FullName  # Full absolute path to markdown file (i.e. input for pandoc)
+$inName = $in_item.Name  # Name of that file, should end in ".md"
 
-$outProvidedCondition = 1 + $verbose #(if ($verbose) { 1 } else { 0 })
-echo $outProvidedCondition
+$outProvidedCondition = 1 + $verbose
+echo "outProvidedCondition: $outProvidedCondition"
 
-$outHtml = "$in" -replace ".md", ".html"
-if ($args.Length -gt $outProvidedCondition) { $outPdf = $args[1] }
-else { $outPdf = "$in" -replace ".md", ".pdf" }
+$outHtmlName = $inName -replace ".md", ".html"
+$outHtml = $outHtml  #"$oldPath/$outHtmlName"
+$outPdf = ""
+
+if ($args.Length -gt $outProvidedCondition) { 
+    $outPdf = $args[1] 
+    if (-not (Split-Path $outPdf -IsAbsolute)) {
+        $outPdf = "$oldPath/$outPdf"  # resolves relative path to output file against the path where the script was called
+    }
+} else {
+    $outPdfName = $inName -replace ".md", ".pdf" 
+    $outPdf = "$oldPath/$outPdfName"  # by defaults saves pdf where script was called
+}
 
 if ($verbose) { echo "Generating course description (html)" }
 
@@ -33,6 +48,10 @@ wkhtmltopdf --enable-local-file-access -T 2 -B 0 -L 3 -R 0  $outHtml $outPdf #2>
 #TODO: maybe do different log files
 
 
-#$outPdf = (Resolve-Path $outPdf).Path 
 if ($verbose) { echo "`nDone.`nPDF is present at `"$outPdf`"" }
-rm $outHtml -ErrorAction SilentlyContinue
+
+if ($outHtml -and (Test-Path $outHtml)) { rm "$outHtml" -ErrorAction SilentlyContinue  #2>> $logFile
+    }
+
+
+cd $oldPath
