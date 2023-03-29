6dd9281d7b4257229616e0167e53f7ca6e671dfe
David Kyrat <33601938+David-Kyrat@users.noreply.github.com>
2023-03-29 08:23:16 +0200

    idem

--- res/templates/fill-all-template-para.ps1
+++ res/templates/fill-all-template-para.ps1
@@ -3,9 +3,9 @@ $scriptPath = Split-Path $MyInvocation.MyCommand.Path -Parent
 
 cd $scriptPath 
 # now we should be at <root-dir>/res/templates
-# $mdDir = (Resolve-Path ("$pwd/../md")).Path
 
 
+# For Each markdown file in res/md/ directory, do in Parallel =>
 ls "../md/*.md" | % -Parallel {
     $outPdfPath = "../pdf/" + $_.Name.replace(".md", ".pdf")
     ./fill-template_convert-pdf.ps1 $_.FullName $outPdfPath -ErrorAction Continue 2>&1> $null
