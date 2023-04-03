d006b1c455765aa4b5761868fc0085a6324dabcd
David Kyrat <33601938+David-Kyrat@users.noreply.github.com>
2023-04-03 15:22:05 +0200

    removed js dir

--- res/templates/js/script8.js
+++
@@ -1,8 +0,0 @@
- $(document).ready(function () {
-     window.buildTabsets("toc");
-   });
-    $(document).ready(function () {
-     $('.tabset-dropdown > .nav-tabs > li').click(function () {
-       $(this).parent().toggleClass('nav-tabs-open')
-     });
-   });
