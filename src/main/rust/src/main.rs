a0b163d4c43cc8f2189d0ac93e98dbbc7363acc3
David Kyrat <33601938+David-Kyrat@users.noreply.github.com>
2023-04-05 07:18:30 +0200

    added rust test file

--- src/main/rust/src/main.rs
+++ src/main/rust/src/main.rs
@@ -1,5 +1,6 @@
-use std::{env, io};
+use std::{env, io, fs};
 use std::path::{PathBuf, Path};
+use std::fs::{ReadDir, DirEntry};
 use path_clean::PathClean;
 use rayon::prelude::*;
 use rayon::iter::ParallelIterator;
@@ -8,6 +9,8 @@ use winsafe::co::CREATE;
 use winsafe::guard::CloseHandlePiGuard;
 use winsafe::{SysResult, HPROCESS, STARTUPINFO, prelude::kernel_Hprocess};
 
+//#[cfg(test)]
+pub mod test;
 
 /// # Description
 /// Creates a process using winsafe api. (safe wrapper around windows sdk api).
@@ -52,24 +55,6 @@ pub fn execvp(app_name: &str, command_line: &str) {
     //println!("after wait");
 }
 
-pub fn test() {
-    let args: Vec<String> = env::args().collect();
-    let mut exe_path: PathBuf = env::current_exe().expect("Could not get executable path");
-    exe_path.pop();
-    exe_path.pop();
-    let pandoc_path: &_ = &exe_path;
-
-    let app_name: &str = pandoc_path.to_str().expect("cannot parse path");
-    let inp_md = &args[1];
-    let out_html: &str = "";
-    let template = "../../desc-template.html";
-    // let cmd_line = &args[1..].join(" ");
-    // let cmd_line = &format!("cc {cmd_line}");
-    let cmd_line: &str = &format!("{inp_md} -t html --template={template} -o {out_html}");
-    dbg!(cmd_line);
-
-    execvp(app_name, cmd_line);
-}
 
 
 pub fn absolute_path(path: impl AsRef<Path>) -> io::Result<PathBuf> {
@@ -120,21 +105,7 @@ fn get_resources_path() -> (String, String, String, String) {
     )
 }
 
-fn test_get_resources_path() {
-    let (pandoc_path, wk_path, md_path, templates_path) = get_resources_path();
-    println!("pandoc_path:\n{:#?}, exists? {}", pandoc_path, Path::new(&pandoc_path).exists());
 
-    println!("--------------------\n");
-    println!("wk_path:\n{:#?}, exists? {}", wk_path, Path::new(&wk_path).exists());
-
-    println!("--------------------\n");
-    println!("md_path:\n{:#?}, exists? {}", md_path, Path::new(&md_path).exists());
-
-    println!("--------------------\n");
-    println!("templates_path:\n{:#?}, exists? {}", templates_path, Path::new(&templates_path).exists());
-}
-
-use std::fs::{self, ReadDir, DirEntry};
 
 
 /// # Description
@@ -264,24 +235,6 @@ pub fn ftcp_parallel(pandoc_path: &str, wk_path: &str, md_path: &str, templates_
         0 => Ok(()),
         _ => Err(std::io::Error::new(io::ErrorKind::Other, err_messages.join("\n")))
     }
-}
-
-
-fn test_ftcp() -> Result<(), String> {
-    let args: Vec<String> = env::args().collect();
-    if args.len() != 2 { 
-        return Err("Expecting 1 argument (name of markdown file in /res/md)".to_string());
-    }
-    // FIX: md input should be only a filename (of a file in res/md/)
-    println!("--------------------\n\n");
-
-    let (pandoc_path, wk_path, md_path, templates_path) = get_resources_path();
-
-    let tmp = fill_template_convert_pdf(&args[1], &pandoc_path, &wk_path, &md_path, &templates_path).unwrap();
-    let out_pdf: &Path = Path::new(&tmp);
-
-    println!("out_pdf:\n{:#?}, exists? {}", out_pdf, out_pdf.exists());
-    Ok(())
 }
 
 
---
+++ src/main/rust/src/test.rs
@@ -0,0 +1,82 @@
+ #![allow(dead_code)] // allowing dead code since this is a test file
+
+use crate::{execvp, fill_template_convert_pdf, ftcp_parallel, get_resources_path};
+use std::{
+    env, io,
+    path::{Path, PathBuf},
+};
+
+pub fn test() {
+    let args: Vec<String> = env::args().collect();
+    let mut exe_path: PathBuf = env::current_exe().expect("Could not get executable path");
+    exe_path.pop();
+    exe_path.pop();
+    let pandoc_path: &_ = &exe_path;
+
+    let app_name: &str = pandoc_path.to_str().expect("cannot parse path");
+    let inp_md = &args[1];
+    let out_html: &str = "";
+    let template = "../../desc-template.html";
+    // let cmd_line = &args[1..].join(" ");
+    // let cmd_line = &format!("cc {cmd_line}");
+    let cmd_line: &str = &format!("{inp_md} -t html --template={template} -o {out_html}");
+    dbg!(cmd_line);
+
+    execvp(app_name, cmd_line);
+}
+
+fn test_get_resources_path() {
+    let (pandoc_path, wk_path, md_path, templates_path) = get_resources_path();
+    println!(
+        "pandoc_path:\n{:#?}, exists? {}",
+        pandoc_path,
+        Path::new(&pandoc_path).exists()
+    );
+
+    println!("--------------------\n");
+    println!(
+        "wk_path:\n{:#?}, exists? {}",
+        wk_path,
+        Path::new(&wk_path).exists()
+    );
+
+    println!("--------------------\n");
+    println!(
+        "md_path:\n{:#?}, exists? {}",
+        md_path,
+        Path::new(&md_path).exists()
+    );
+
+    println!("--------------------\n");
+    println!(
+        "templates_path:\n{:#?}, exists? {}",
+        templates_path,
+        Path::new(&templates_path).exists()
+    );
+}
+
+fn test_ftcp() -> Result<(), String> {
+    let args: Vec<String> = env::args().collect();
+    if args.len() != 2 {
+        return Err("Expecting 1 argument (name of markdown file in /res/md)".to_string());
+    }
+    // WARN: md input should be only a filename (of a file in res/md/)
+    println!("--------------------\n\n");
+
+    let (pandoc_path, wk_path, md_path, templates_path) = get_resources_path();
+
+    let tmp =
+        fill_template_convert_pdf(&args[1], &pandoc_path, &wk_path, &md_path, &templates_path)
+            .unwrap();
+    let out_pdf: &Path = Path::new(&tmp);
+
+    println!("out_pdf:\n{:#?}, exists? {}", out_pdf, out_pdf.exists());
+    Ok(())
+}
+
+fn test_ftcp_parallel() -> io::Result<()> {
+    let (pandoc_path, wk_path, md_path, templates_path) = get_resources_path();
+
+    let out = ftcp_parallel(&pandoc_path, &wk_path, &md_path, &templates_path);
+    out
+}
