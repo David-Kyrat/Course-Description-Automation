0ec251151efe0e4c7a21e758e49460868606b2db
David Kyrat <33601938+David-Kyrat@users.noreply.github.com>
2023-04-26 20:27:40 +0200

    cargo fix

--- src/main/rust/src/main.rs
+++ src/main/rust/src/main.rs
@@ -5,19 +5,7 @@ pub mod win_exec;
 
 use utils::{abs_path_clean, init_log4rs, pop_n_push_s};
 
-
-
-
-
-
-use std::{io};
-
-
-
-
-
-
-
+use std::io;
 
 pub mod test_exevp_out;
 
@@ -26,10 +14,9 @@ pub fn main() -> io::Result<()> {
     //init_log4rs(None);
     // HK: DONT DELETE ABOVE THIS
 
-    log4rs::init_file("logging_config.yaml", Default::default());
-
+    log4rs::init_file("logging_config.yaml", Default::default()).expect("Cannot find log file");
 
     // _main()
-    
+
     test_exevp_out::test_main()
 }
