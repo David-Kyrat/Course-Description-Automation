42cd95efff4b455a31c9e012146688716ea9bb0e
David Kyrat <33601938+David-Kyrat@users.noreply.github.com>
2023-04-26 20:26:13 +0200

    Update

--- src/main/rust/src/test_exevp_out.rs
+++ src/main/rust/src/test_exevp_out.rs
@@ -1,27 +1,39 @@
-
-use crate::win_exec::execvp_out;
-use crate::{abs_path_clean, init_log4rs, pop_n_push_s, execvp, get_resources_path, RETRY_AMOUNT};
+#![allow(unused)]
+#![feature(windows_process_extensions_async_pipes)]
+// use crate::{abs_path_clean, execvp, get_resources_path, init_log4rs, pop_n_push_s, RETRY_AMOUNT};
 
 use io::ErrorKind::Other;
 use rayon::iter::*;
 use std::fs::{DirEntry, ReadDir};
 use std::path::{Path, PathBuf};
+use std::process::Output;
 use std::{env, fs, io};
 
 use winsafe::co::CREATE;
 use winsafe::guard::CloseHandlePiGuard;
 use winsafe::{prelude::*, SysResult, HPIPE, HPROCESS, STARTUPINFO};
 
-
 use log::{error, warn};
 
 pub fn test_main() -> io::Result<()> {
-
-    let executable = "C:\\Users\\noahm\\bin\\wkhtmltopdf.exe";
+    /* let executable = "C:\\Users\\noahm\\bin\\wkhtmltopdf.exe";
     let command_line = "-h";
-    let out = execvp_out(executable, command_line, Some(u32::MAX));
-    dbg!(out);
-    
+    let out = execvp(executable, command_line, Some(u32::MAX));
+    dbg!(out); */
+    let executable = "C:\\Users\\noahm\\bin\\wkhtmltopdf.exe";
+
+    use std::os::windows::process::CommandExt;
+    use std::process::{Command, Stdio};
+
+    let program = executable;
+    let output: Output = Command::new(executable)
+        // .args(["/C", "echo hello"])
+        .args(&["-h"])
+        .output()
+        .expect("failed to execute process");
+    let out_str = &String::from_utf8(output.stdout).unwrap();
+    dbg!(out_str);
+    println!("output: {}", out_str);
+
     Ok(())
 }
-
