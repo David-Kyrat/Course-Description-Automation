2120159b33775a672b3af259114d6d6da149723f
David Kyrat <33601938+David-Kyrat@users.noreply.github.com>
2023-06-05 18:04:42 +0200

    fixed name of utils.rs

--- src/main/rust/src/main.rs
+++ src/main/rust/src/main.rs
@@ -1,4 +1,5 @@
 #![allow(non_snake_case)]
+
 extern crate native_windows_derive as nwd;
 extern crate native_windows_gui as nwg;
 
@@ -12,7 +13,6 @@ use utils::{abs_path_clean, init_log4rs, pop_n_push_s, init_log4rs_debug};
 
 
 pub fn main() {
-    // FIX:: ADD BACK BELOW WHEN DONE TESTING
     init_log4rs(None);
     // HK: DONT DELETE ABOVE THIS
 
