0bdf6e730d7724b21090f8f6c716e6cbf8ec88cd
David Kyrat <33601938+David-Kyrat@users.noreply.github.com>
2023-06-05 17:47:20 +0200

    Final Jar for scala part (v1.2)

--- src/windows/main/scala/ch/net/exception/CourseNotFoundException.scala
+++
@@ -1,4 +0,0 @@
-package ch.net.exception
-
-case class CourseNotFoundException(private val message: String)
-        extends Exception(ResourceNotFoundException.fmtErr("course \"" + message.split("-")(1) + "\"")) with ResourceNotFoundException
