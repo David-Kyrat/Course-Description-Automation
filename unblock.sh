48275d909b076280f7e693bd694368a0879cc372
David Kyrat <33601938+David-Kyrat@users.noreply.github.com>
2023-06-06 21:27:55 +0200

    Update unblock.sh

--- unblock.sh
+++ unblock.sh
@@ -1,3 +1,9 @@
 #!/bin/sh
 
-
+# Attempt at tricking os into thinking file has not be downloaded from the internet
+# to be able to just run it...
+file="$0"
+cat "$file" > temp
+cat temp > "$file"
+chmod u+x "$file"
+rm temp
