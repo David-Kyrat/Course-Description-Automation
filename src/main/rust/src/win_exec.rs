2be999672917bbcc5194c3b79ecb52b790e334ce
David Kyrat <33601938+David-Kyrat@users.noreply.github.com>
2023-06-06 17:09:51 +0200

    added unused rust scripts for windows execvp and gui popup handling if a windows implementation is ever needed someday

---
+++ src/main/rust/src/win_exec.rs
@@ -0,0 +1,108 @@
+// NOTE: Unused since the client's OS isn't windows but this file is kept as reference in case a
+// windows implementation is, one day, needed. (dependency is winsafe api see "https://crates.io/crates/WinSafe")
+
+/*
+#![allow(non_snake_case)]
+#![allow(unused)]
+
+use crate::{abs_path_clean, init_log4rs, pop_n_push_s, fr};
+
+use io::ErrorKind::Other;
+use rayon::iter::*;
+use std::fs::{DirEntry, ReadDir};
+use std::path::{Path, PathBuf};
+use std::{env, fs, io};
+
+use winsafe::prelude::{kernel_Hpipe, kernel_Hstd};
+use winsafe::{prelude::*, SECURITY_DESCRIPTOR};
+use winsafe::{co::*, HFILE};
+use winsafe::guard::CloseHandlePiGuard;
+use winsafe::{prelude::*, SysResult, HPIPE, HPROCESS, STARTUPINFO, SECURITY_ATTRIBUTES, PROCESS_INFORMATION};
+
+use log::{error, warn};
+/// # Returns
+/// `io::Error::new(Other, message)`. i.e. a custom `io::Error`
+fn custom_io_err(message: &str) -> io::Error {
+    io::Error::new(Other, message)
+}
+
+
+/// # Description
+/// Private wrapper around `HPROCESS::CreateProcess`
+///
+/// # Return
+/// `SysResult<CloseHandlePiGuard>` (the result of `HPROCESS::CreateProcess` this function is
+/// really just a wrapper around that function.)
+fn exec(app_name: &str, command_line: &str) -> SysResult<CloseHandlePiGuard> {
+    let (app_name, command_line) = (app_name.trim(), command_line.trim());
+    // NOTE: If command has no arguments (i.e. `command_line == ""`) then
+    // command_line_opt should be Some(app_name) and
+    // app_name_opt should be none (because in reality `command_line` is argv)
+    let app_name_opt = match command_line {
+        "" => None,
+        _ => Some(app_name),
+    };
+    let command_line: &str = &format!("{app_name} {command_line}");
+    let command_line = command_line.trim();
+    let cmd_line_opt = Some(command_line);
+    // first word before space in command line should be app_name
+    // (it is ignored either way if app_name is not None because its argv[0])
+    HPROCESS::CreateProcess(
+        app_name_opt,
+        cmd_line_opt,
+        None,
+        None,
+        true,
+        CREATE::NO_WINDOW | CREATE::INHERIT_PARENT_AFFINITY,
+        None, //inherits
+        None, // inherits
+        &mut STARTUPINFO::default())
+}
+
+
+
+/// # Description
+/// Creates a process using winsafe api. (safe wrapper around windows sdk api).
+/// On Windows, Creating a new process always comes with the execution of some executable in a new
+/// thread / in that child process (there is no "just" fork.)
+/// That's why this function take an absolute_path to an executable
+/// and the the arguments to pass to it (argv)
+///
+/// # Params
+/// - `app_name`: Absolute path to an executable
+/// - `command_line`: Argument to program (equivalent of `argv`)
+/// - `wait_time`: time to wait for the process to finish (in milliseconds)
+///
+/// # NB
+/// This functions returns after having waited on the "child" process.
+/// (Although the wait is not mandatory to avoid zombies thanks to the winsafe api,
+/// here we just want to wait for the completion of the job.)
+pub fn execvp(app_name: &str, command_line: &str, wait_time: Option<u32>) -> io::Result<()> {
+    let close_handle_res = exec(app_name, command_line);
+
+    if close_handle_res.is_err() {
+        return Err(custom_io_err(&format!(
+            "WinErr: could not start process '{app_name} {command_line}', {}.   Line {}, File '{}'",
+            close_handle_res.map(|_| ()).unwrap_err(),
+            line!(),
+            file!()
+        )));
+    }
+
+    let wait_time = match wait_time {
+        Some(amount) => Some(amount),
+        None => Some(10_000), //10 sec by default
+    };
+
+    let close_handle = close_handle_res.unwrap();
+    let wait_res = HPROCESS::WaitForSingleObject(&close_handle.hProcess, wait_time); // waits 10 sec at most
+                                                                                        // its ok to wait this long because the calls are often made in parallel so they're not actually blocking each other
+    if wait_res.is_err() {
+        warn!(
+            "Could not wait on child process: {app_name} {command_line}\n\t{}",
+            wait_res.unwrap_err()
+        );
+    }
+    Ok(())
+}
+*/
---
+++ src/main/rust/src/win_popup.rs
@@ -0,0 +1,138 @@
+// unused since crate native-dialog can do the same a lot easier and cross OS
+
+/* use nwd::NwgUi;
+use nwg::MessageButtons as MB;
+use nwg::MessageChoice as MC;
+use nwg::MessageIcons as MI;
+use nwg::{ControlHandle,  MessageChoice, NativeUi};
+
+
+#[derive(Default, NwgUi)]
+pub struct EmbedApp {
+    #[nwg_control(size: (0, 0), position: (300, 300), flags: "VISIBLE|POPUP", topmost: true, center: true)]
+    #[nwg_events( OnWindowClose: [EmbedApp::exit], OnInit: [EmbedApp::init] )]
+    window: nwg::Window,
+    #[nwg_resource]
+    embed: nwg::EmbedResource,
+    #[nwg_resource(source_embed: Some(&data.embed), source_embed_str: Some("WINDOWICON"))]
+    window_icon: nwg::Icon,
+}
+
+fn set_window_icon(ea: &EmbedApp) {
+    ea.window.set_icon(Some(&ea.window_icon));
+}
+
+fn modal(parent: ControlHandle, _title: &str, _content: &str, success: bool) -> nwg::MessageChoice {
+    let p = nwg::MessageParams {
+        title: _title,
+        content: _content,
+        buttons: if success { MB::YesNo } else { MB::RetryCancel },
+        icons: if success { MI::Info } else { MI::Error },
+    };
+
+    nwg::modal_message(parent, &p)
+}
+
+fn handle_match(choice: MessageChoice) -> bool {
+    match choice {
+        MC::Retry | MC::Yes =>  true,
+        MC::Cancel | MC::No =>  false,
+        _ => { panic!("unexpected button clicked"); }
+    }
+}
+
+impl EmbedApp {
+    pub const TITLE_SUCC: &str = "PDF generation successful";
+    pub const TITLE_ERR: &str = "PDF generation unsuccessful";
+    pub const MESSAGE_SUCC: &str =
+        "PDF generation successful.\n Would you like to generate anything more?";
+    pub const FONT: &str = "Microsoft Sans Serif";
+
+    pub fn message_err(error: String) -> String {
+        format!("{} \nDo you want to retry?", error)
+    }
+
+    fn main_win(&self, success: bool, err_msg: Option<String>) -> bool {
+        let err_msg = match err_msg {
+            Some(expr) => expr,
+            None => { if success { "".to_owned() } else { "An unexpected error happened".to_owned() } }
+        };
+
+        let (title, message) = if success {
+            (EmbedApp::TITLE_SUCC.to_owned(), EmbedApp::MESSAGE_SUCC.to_owned())
+        } else {
+            (EmbedApp::TITLE_ERR.to_owned(), EmbedApp::message_err(err_msg.to_owned()))
+        };
+
+        self.window.set_text(title.as_str());
+        let choice = modal(self.window.handle, title.as_str(), message.as_str(), success);
+        let restart = handle_match(choice);
+        self.exit();
+        restart
+    }
+
+    /// # Returns
+    /// Retry (restart app) or cancel (just quits)
+    pub fn success_window(&self) -> bool {
+        let choice = modal(
+            self.window.handle,
+            EmbedApp::TITLE_SUCC,
+            EmbedApp::MESSAGE_SUCC,
+            true,
+        );
+        let restart = handle_match(choice);
+        self.exit();
+        restart
+    }
+
+    /// # Returns
+    /// Retry (restart app) or cancel (just quits)
+    pub fn error_window(&self, error_msg: String) -> bool {
+        let choice = modal(
+            self.window.handle,
+            EmbedApp::TITLE_ERR,
+            &EmbedApp::message_err(error_msg),
+            false,
+        );
+        let restart = handle_match(choice);
+        self.exit();
+        restart
+    }
+
+    pub fn init(&self) {
+        // self.window.set_text(EmbedApp::TITLE_SUCC);
+        set_window_icon(self);
+        self.window.set_focus();
+    }
+
+    fn exit(&self) {
+        nwg::stop_thread_dispatch();
+    }
+}
+
+pub fn main(success: bool, err_msg: Option<String>) -> bool {
+    nwg::init().expect("Failed to init Native Windows GUI");
+    let app = EmbedApp::build_ui(Default::default()).expect("Failed to build UI");
+    let response = app.main_win(success, err_msg);
+    nwg::dispatch_thread_events();
+    response
+}
+
+/*
+fn get_font(font_name: &str, size: u32) -> Font {
+    let mut font = Default::default();
+    let res = nwg::Font::builder()
+        .family(font_name)
+        .size(size)
+        .build(&mut font);
+    if res.is_err() {
+        nwg::Font::builder()
+            .family("Arial")
+            .size(size)
+            .build(&mut Default::default())
+            .expect("Arial (Default font) not available on your system");
+    } else {
+        res.unwrap();
+    }
+    font
+} */ */
