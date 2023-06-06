cec2529623b5fcbd205a3ec405c953311104fc7d
David Kyrat <33601938+David-Kyrat@users.noreply.github.com>
2023-06-06 21:00:33 +0200

    added back wkhtmltopdf for mac

---
+++ updatejar.sh
@@ -0,0 +1,5 @@
+#!/bin/zsh
+
+jar_url="https://github.com/David-Kyrat/Course-Description-Automation/raw/scalight/target/scala-2.13/course-description-automation.jar"
+
+wget  "$jar_url" -O ./files/res/java/course-description-automation.jar
