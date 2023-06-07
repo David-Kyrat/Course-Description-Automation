f5ce64ccb7bf9f1be76e72f266b11268635c8e5c
David Kyrat <33601938+David-Kyrat@users.noreply.github.com>
2023-06-07 20:41:55 +0200

    fixed typo in utils.rs and main.scala

--- src/main/rust/src/main.rs
+++ src/main/rust/src/main.rs
@@ -10,7 +10,8 @@ use utils::{abs_path_clean, init_log4rs, pop_n_push_s};
 pub fn main() {
     init_log4rs(None);
     // HK: DONT DELETE ABOVE THIS
-
     // init_log4rs_debug();
+    let paths = utils::current_exe_path();
+    dbg!(paths);
     launcher::main();
 }
